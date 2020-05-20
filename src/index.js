4d5215790e71faaeb09ab6ec872c1f0773ecc368
anseelejulia <julia.anseele@student.howest.be>
2020-05-20 16:48:09 +0200

    added js counter

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import './style.css';
     addListenersToSteveParts();
   };
 
+
   const addListenersToSteveParts = () => {
     const allbuttons = document.querySelectorAll('.grid__button');
     console.log(allbuttons);
@@ -12,9 +13,10 @@ import './style.css';
     const interval = 3000;
     const swapImage = id => {
       document.getElementById(id).setAttribute('src', 'assets/gifs/main.gif');
-      console.log(id, 'tot hier is fine');
-
+      // console.log(id, 'tot hier is fine');
     };
+
+    let amountClicks = 0;
 
     allbuttons.forEach(button => {
       button.addEventListener('click', e => {
@@ -22,12 +24,16 @@ import './style.css';
         const currentIndex = button.id;
 
         if (currentIndex == 1) {
+          amountClicks++;
+          console.log(amountClicks);
           console.log(`nummer 1 is aangeduid`);
           const $gettingimgsrc = document.querySelector(`.steveoriginal`);
           $gettingimgsrc.src = 'assets/gifs/main.gif';
           setInterval(swapImage(1), interval);
 
         } else if (currentIndex == 2) {
+          amountClicks++;
+          console.log(amountClicks);
           console.log(`nummer 2 is aangeduid`);
           const $gettingimgsrc = document.querySelector(`.steveoriginal`);
           $gettingimgsrc.src = 'assets/gifs/eyeright.gif';
@@ -35,6 +41,8 @@ import './style.css';
 
 
         } else if (currentIndex == 3) {
+          amountClicks++;
+          console.log(amountClicks);
           console.log(`nummer 3 is aangeduid`);
           const $gettingimgsrc = document.querySelector(`.steveoriginal`);
           $gettingimgsrc.src = 'assets/gifs/armlinks1_1.gif';
@@ -42,6 +50,8 @@ import './style.css';
 
 
         } else if (currentIndex == 4) {
+          amountClicks++;
+          console.log(amountClicks);
           console.log(`nummer 4 is aangeduid`);
           const $gettingimgsrc = document.querySelector(`.steveoriginal`);
           $gettingimgsrc.src = 'assets/gifs/main.gif';
@@ -49,6 +59,8 @@ import './style.css';
 
 
         } else if (currentIndex == 5) {
+          amountClicks++;
+          console.log(amountClicks);
           console.log(`nummer 5 is aangeduid`);
           const $gettingimgsrc = document.querySelector(`.steveoriginal`);
           $gettingimgsrc.src = 'assets/gifs/armlinks2_3.gif';
@@ -56,27 +68,37 @@ import './style.css';
 
 
         } else if (currentIndex == 6) {
+          amountClicks++;
+          console.log(amountClicks);
           console.log(`nummer 6 is aangeduid`);
           const $gettingimgsrc = document.querySelector(`.steveoriginal`);
           $gettingimgsrc.src = 'assets/gifs/main.gif';
           setInterval(swapImage(6), interval);
 
         } else if (currentIndex == 7) {
+          amountClicks++;
+          console.log(amountClicks);
           console.log(`nummer 7 is aangeduid`);
           const $gettingimgsrc = document.querySelector(`.steveoriginal`);
           $gettingimgsrc.src = 'assets/gifs/armlinks3_1.gif';
 
         } else if (currentIndex == 8) {
+          amountClicks++;
+          console.log(amountClicks);
           console.log(`nummer 8 is aangeduid`);
           const $gettingimgsrc = document.querySelector(`.steveoriginal`);
           $gettingimgsrc.src = 'assets/gifs/main.gif';
 
         } else if (currentIndex == 9) {
+          amountClicks++;
+          console.log(amountClicks);
           console.log(`nummer 9 is aangeduid`);
           const $gettingimgsrc = document.querySelector(`.steveoriginal`);
           $gettingimgsrc.src = 'assets/gifs/main.gif';
 
         } else if (currentIndex == 10) {
+          amountClicks++;
+          console.log(amountClicks);
           console.log(`nummer 10 is aangeduid`);
           const $gettingimgsrc = document.querySelector(`.steveoriginal`);
           $gettingimgsrc.src = 'assets/gifs/voetrechts.gif';
@@ -86,6 +108,12 @@ import './style.css';
         }
       });
     });
+
+    if(amountClicks > 3){
+      console.log('owch he is done with ur sht');
+      const $gettingimgsrc = document.querySelector(`.steveoriginal`);
+      $gettingimgsrc.src ="assets/gifs/eyeright";
+    };
   };
 
   init();
