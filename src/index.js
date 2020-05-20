826df60edaa4bd47138152deb4844b7aa5512524
anseelejulia <julia.anseele@student.howest.be>
2020-05-20 13:33:03 +0200

    correction js

--- src/index.js
+++ src/index.js
@@ -16,36 +16,36 @@ import './style.css';
 
         if(currentIndex == 1){
           console.log(`nummer 1 is aangeduid`);
-          const $gettingimgsrc = document.querySelector(`.grid__button1`);
+          const $gettingimgsrc = document.querySelector(`.steveoriginal`);
           $gettingimgsrc.src = "voorbeeld1.gif";
 
         } else if(currentIndex ==2){
           console.log(`nummer 2 is aangeduid`);
-          const $gettingimgsrc = document.querySelector(`.grid__button2`);
+          const $gettingimgsrc = document.querySelector(`.steveoriginal`);
           $gettingimgsrc.src= 'voorbeeld2.gif';
 
 
         } else if(currentIndex ==3){
           console.log(`nummer 3 is aangeduid`);
-          const $gettingimgsrc = document.querySelector(`.grid__button3`);
+          const $gettingimgsrc = document.querySelector(`.steveoriginal`);
           $gettingimgsrc.src= 'sdqsd';
 
 
         } else if(currentIndex ==4){
           console.log(`nummer 4 is aangeduid`);
-          const $gettingimgsrc = document.querySelector(`.grid__button4`);
+          const $gettingimgsrc = document.querySelector(`.steveoriginal`);
           $gettingimgsrc.src= 'fezfze';
 
         
         } else if(currentIndex ==5){
           console.log(`nummer 5 is aangeduid`);
-          const $gettingimgsrc = document.querySelector(`.grid__button5`);
+          const $gettingimgsrc = document.querySelector(`.steveoriginal`);
           $gettingimgsrc.src= 'fzef';
 
         
         } else if(currentIndex == 6){
           console.log(`nummer 6 is aangeduid`);
-          const $gettingimgsrc = document.querySelector(`.grid__button6`);
+          const $gettingimgsrc = document.querySelector(`.steveoriginal`);
           $gettingimgsrc.src= 'zefzef';
 
         } else {
